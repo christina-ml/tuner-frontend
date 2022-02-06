9ba21cc4b69821fcc7186230abd07efe51241bce
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-06 14:11:43 -0500

    added Index page - Songs.js

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,10 +1,40 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Song from "./Song";
 
+const API = process.env.REACT_APP_API_URL;
+
 export default function Songs() {
+    const [songs, setSongs] = useState([]);
+
+    useEffect(()=>{
+        axios.get( API + "/songs" )
+            .then((res)=>{
+                setSongs(res.data);
+            }).catch((err)=>{
+                console.log(err);
+            })
+    }, []);
+
     return (
-        <div>
+        <div className="Songs">
+            <section>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Take me there</th>
+                            <th>See this song</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {songs.map((song)=>{
+                            return <Song key={song.id} song={song} />
+                        })}
+                    </tbody>
+                </table>
+            </section>
             Songs page
-            <Song />
         </div>
     )
   }
