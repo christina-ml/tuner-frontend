a05587fbca84859afe541e0873c2ef262d46b391
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-06 20:03:01 -0500

    added SongNewForm

--- src/components/SongNewForm.js
+++ src/components/SongNewForm.js
@@ -1,7 +1,93 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const API = process.env.REACT_APP_API_URL;
+
 export default function SongNewForm() {
+    let navigate = useNavigate();
+
+    const addSong = (newSong) => {
+        axios
+            .post(`${API}/songs`, newSong)
+            .then(
+                () => {
+                navigate(`/songs`);
+                },
+                (error) => console.error(error)
+            )
+            .catch((c) => console.warn("catch", c));
+    };
+
+    const [song, setSong] = useState({
+        name: "",
+        artist: "",
+        album: "",
+        time: "",
+        is_favorite: false,
+    })
+
+    const handleTextChange = (event) => {
+        setSong({ ...song, [event.target.id]: event.target.value });
+    }
+
+    const handleCheckboxChange = () => {
+        setSong({ ...song, is_favorite: !song.is_favorite });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addSong(song);
+        navigate(`/songs`);
+    }
+
     return (
-        <div>
-            Song New Form page
+        <div className="New">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input 
+                    id="name" 
+                    value={song.name}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Name of Song"
+                    required
+                />
+                <label htmlFor="artist">Artist:</label>
+                <input 
+                    id="artist" 
+                    value={song.artist}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Name of Artist"
+                />
+                <label htmlFor="album">Album:</label>
+                <input 
+                    id="album" 
+                    value={song.album}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Name of Album"
+                />
+                <label htmlFor="time">Time:</label>
+                <input 
+                    id="time" 
+                    value={song.time}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Length of Time"
+                />
+                <label htmlFor="is_favorite">Favorite:</label>
+                <input 
+                    id="is_favorite"
+                    type="checkbox"
+                    onChange={handleCheckboxChange}
+                    checked={song.is_favorite}
+                />
+
+                <br />
+                <input type="submit" />
+            </form>
         </div>
     )
   }
